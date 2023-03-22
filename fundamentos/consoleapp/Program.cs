af9e753339e6a57cc66df37c66f6f0b4256ac0b2
Sebastian <1144105003@u.icesi.edu.co>
2023-03-21 19:59:54 -0500

    Update of console app

--- fundamentos/consoleapp/Program.cs
+++ fundamentos/consoleapp/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter your name");
+var name = Console.ReadLine();
+Console.WriteLine("Enter your job");
+var job = Console.ReadLine();
+Console.WriteLine("Enter your age");
+var age = Console.ReadLine();
+
+Console.WriteLine($"My name is {name}, mi job is {job} and my age is {age}");
+
