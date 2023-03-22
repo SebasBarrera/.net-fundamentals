3e74ae772340bf94633e3b76a1d811909c426d43
Sebastian <1144105003@u.icesi.edu.co>
2023-03-21 20:40:44 -0500

    Using humanizer library, downloaded from NuGet

--- fundamentos/consoleapp/Program.cs
+++ fundamentos/consoleapp/Program.cs
@@ -1,10 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Humanizer;
 Console.WriteLine("Enter your name");
 var name = Console.ReadLine();
 Console.WriteLine("Enter your job");
 var job = Console.ReadLine();
 Console.WriteLine("Enter your age");
-var age = Console.ReadLine();
+var age = int.Parse(Console.ReadLine());
 
-Console.WriteLine($"My name is {name}, mi job is {job} and my age is {age}");
+
+Console.WriteLine($"My name is {name}, mi job is {job} and my age is {age.ToWords()}. Thanks");
 
